31ea7a98cca10d84f38a380f44512afbe0f7b4df
Ryan Wilkins <wilkins.ryan@gmail.com>
2017-08-25 13:46:49 -0700

    Use nokogiri gem to focus on table elements

--- TEST2/test2.rb
+++ TEST2/test2.rb
@@ -1,9 +1,9 @@
 # http://ruby-doc.org/stdlib-2.0.0/libdoc/open-uri/rdoc/OpenURI.html 
 require 'open-uri'
+require 'nokogiri'
 
 # Go fetch the contents of a URL & store them as a String
-response = open('http://www2.stat.duke.edu/courses/Spring01/sta114/data/andrews.html').read
+response = open('http://www2.stat.duke.edu/courses/Spring01/sta114/data/andrews.html')
+doc = Nokogiri::HTML(response)
 
-# "Pretty prints" the result to look like a web page instead of one long string of HTML
-# URI.parse(response).class
-print response
+puts doc.xpath('//table')
