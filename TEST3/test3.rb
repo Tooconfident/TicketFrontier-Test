023162b693cb1a7422bb8ed625dc7e467a17a777
Ryan Wilkins <wilkins.ryan@gmail.com>
2017-08-25 20:33:46 -0700

    Focus on year column in file records

--- TEST3/test3.rb
+++ TEST3/test3.rb
@@ -1,8 +1,13 @@
 def process(document)
+	result = []
 	file = document
+	columns = []
 	File.readlines(file).each do |line|
-		puts line
+		records = line.split(' ')		
+		year = records[6]
+		p year
 	end
+	
 end
 
 process("T08 (1).1")
