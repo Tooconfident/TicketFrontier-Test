4084180f91a7adc1413f290a85aeacaba7e55f75
Ryan Wilkins <wilkins.ryan@gmail.com>
2017-08-28 11:45:22 -0700

    Convert file to manageable array

--- TEST3/test3.rb
+++ TEST3/test3.rb
@@ -3,29 +3,32 @@ require 'date'
 class Record
   # Looking at the people.csv file,
   # what attributes should a Person object have?
-  attr_reader :table_number, :sub_table, :record_number, :day_of_week, :day_of_month, :month, :year, :total_deaths, :wounded
+  # attr_reader :table_number, :sub_table, :record_number, :day_of_week, :day_of_month, :month, :year, :total_deaths, :wounded
 
-  def initialize(args = {})
-    @table_number = args[:table_number]
-    @sub_table = args[:sub_table]
-    @record_number = args[:record_number]
-    @day_of_week = args[:day_of_week]
-    @day_of_month = args[:day_of_month]
-    @month = args[:month]
-    @year = args[:year]
-    @total_deaths = args[:total_deaths]
-    @wounded = args[:wounded]
-  end
+  # def initialize(args = {})
+  #   @table_number = args[:table_number]
+  #   @sub_table = args[:sub_table]
+  #   @record_number = args[:record_number]
+  #   @day_of_week = args[:day_of_week]
+  #   @day_of_month = args[:day_of_month]
+  #   @month = args[:month]
+  #   @year = args[:year]
+  #   @total_deaths = args[:total_deaths]
+  #   @wounded = args[:wounded]
+  # end
 
   def process(file)
-	columns = []
-	File.readlines(file).each do |row|
-		row[:year] = 	
-		columns << @year
-	end
-	p columns
+   array = []
+	 File.open(file) do |f|
+		  f.each_line do |line|
+        array << line.split.map(&:to_i)
+        # puts line.class
+      end
+      # p array[6]
+	 end
   end
 end
 
+
 test = Record.new
-test.process("T08 (1).1")
+test.process("T08.1")
